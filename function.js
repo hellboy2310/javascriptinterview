7e994b027c36aeafc63dd6d7f10a49e4de0ffd6c
hellboy2310 <bhaveshpurohit2310@gmail.com>
2022-06-23 12:57:41 +0530

    function statement

---
+++ function.js
@@ -0,0 +1,17 @@
+//types of function 
+//function statement
+
+function sayHello(){
+    console.log("hello world");
+
+}
+sayHello();//function invocation
+
+function sayHi(param){
+    console.log("this is john snow and his age is:-",param);
+}
+sayHi("hello");//function invocation
+
+
+let rval = sayHi();
+console.log("rval",rval);

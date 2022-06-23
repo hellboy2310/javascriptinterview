dde605407972fbafeedb82dcdbf4714a0e9b9cb7
hellboy2310 <bhaveshpurohit2310@gmail.com>
2022-06-23 14:31:52 +0530

    function completed

--- function.js
+++ function.js
@@ -1,17 +1,77 @@
-//types of function 
-//function statement
+// //types of function 
+// //function statement
 
-function sayHello(){
-    console.log("hello world");
+// function sayHello(){
+//     console.log("hello world");
 
+// }
+// sayHello();//function invocation
+
+// function sayHi(param){
+//     console.log("this is john snow and his age is:-",param);
+// }
+// sayHi("hello");//function invocation
+
+
+// let rval = sayHi();
+// console.log("rval",rval);
+
+
+// //function expressino
+// let fnContiner = function fn()
+// {
+//     console.log("this is and expression function");
+// }
+// fnContiner();
+
+// //anonymous function
+
+// let anonymousfunction = function(){
+//     console.log("this is an anonymous function");
+// }
+// anonymousfunction();
+
+// //Immediately invoked function expression
+// (function fn()
+// {
+//     console.log("this is and IIFE function");
+//     console.log("this will run on its own");
+// })();
+
+
+
+// //arrow function
+
+// let fn = (num) =>{
+//     return num * num;
+    
+// }
+// console.log(fn(5));
+
+// variable can be passed as a parameter
+// function sayHello(param){
+//         console.log("hello",param);
+//         param();
+//         return "kuch bi";
+// }
+// function smaller(){
+//     console.log("this is smaller");
+
+// }
+
+
+// // sayHello([1,2,3,4]);
+// let rval = sayHello(smaller);
+// console.log(rval);
+
+
+//RETURNING FUNCTION INSIDE A FUNCTION 
+function outer (){
+    console.log(" i will return inner function");
+    return function inner(){
+        console.log("i am inner function");
+    }
 }
-sayHello();//function invocation
-
-function sayHi(param){
-    console.log("this is john snow and his age is:-",param);
-}
-sayHi("hello");//function invocation
-
-
-let rval = sayHi();
-console.log("rval",rval);
+let rval = outer();
+console.log("Rval",rval);
+rval();

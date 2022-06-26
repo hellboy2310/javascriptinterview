2aa0188232a06d4ac1d72ec95e645e3f388adea2
hellboy2310 <bhaveshpurohit2310@gmail.com>
2022-06-27 00:10:08 +0530

    array

---
+++ array.js
@@ -0,0 +1,34 @@
+// let arr = [];
+// console.log(arr);
+
+let eleArr = [1,23,4,5,"hello this is hellboy"]
+// console.log(eleArr);
+
+// console.log("element at 3rd index",eleArr[3]);
+
+// eleArr[3] = "Nothing";
+// console.log(eleArr);
+
+// 1.push
+// console.log("Array:before push",eleArr);
+// eleArr.push("new item");
+// console.log("Array after push",eleArr);
+
+
+// 2 pop
+// console.log("Array before pop",eleArr);
+// eleArr.pop();
+// console.log("array after pop",eleArr);
+
+
+// // 3 shift
+// console.log("array before shift",eleArr);
+// eleArr.shift();
+// console.log("array after shift",eleArr);
+
+
+
+//unshift
+console.log("array before unshift",eleArr);
+eleArr.unshift("newly added items");
+console.log("array after unshift",eleArr);

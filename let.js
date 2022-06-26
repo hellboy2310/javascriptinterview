70d4ae0c24338242ec369b7a4a2839464315a1b9
hellboy2310 <bhaveshpurohit2310@gmail.com>
2022-06-26 22:33:35 +0530

    let

---
+++ let.js
@@ -0,0 +1,12 @@
+
+//tdz
+//cannot access variable before initialization
+
+console.log("hello");
+
+console.log("line number 6",varName);
+let varName;
+console.log("line number 7",varName);
+varName = 10;
+varName = 20;
+console.log("line number 11",varName);

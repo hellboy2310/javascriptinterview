69c200bc53d19ae9606773d0c67a93943ef43b24
hellboy2310 <bhaveshpurohit2310@gmail.com>
2022-06-26 22:52:19 +0530

    legel and Illegeal shadowing

--- block.js
+++ block.js
@@ -1,14 +1,38 @@
-// {} -> block
+// // {} -> block
 
+// let fruits = "apple";
+// console.log(fruits);
+// {
+//     let fruits;
+//     console.log(fruits);
+//     fruits = "orange";
+//     {
+//         console.log(fruits);
+//     }
+//     console.log(fruits);
+// }
+// console.log(fruits);
+
+
+
+//shadowing 
+//legal shadowing
+
+// let fruits = "apple";
+// console.log(fruits);
+// {
+//     const fruits = "orange";
+//     console.log(fruits);
+// }
+// console.log(fruits);
+
+
+//illegal shadowing
 let fruits = "apple";
 console.log(fruits);
 {
-    let fruits;
-    console.log(fruits);
-    fruits = "orange";
-    {
-        console.log(fruits);
-    }
+    var fruits = "orange";
     console.log(fruits);
 }
 console.log(fruits);
+

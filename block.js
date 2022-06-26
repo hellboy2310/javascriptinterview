476cad97ef4ac39875402078a621a5fd124b65c7
hellboy2310 <bhaveshpurohit2310@gmail.com>
2022-06-26 22:44:25 +0530

    how let works in block

---
+++ block.js
@@ -0,0 +1,14 @@
+// {} -> block
+
+let fruits = "apple";
+console.log(fruits);
+{
+    let fruits;
+    console.log(fruits);
+    fruits = "orange";
+    {
+        console.log(fruits);
+    }
+    console.log(fruits);
+}
+console.log(fruits);

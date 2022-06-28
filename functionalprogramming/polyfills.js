e87304a75a3714eb1a354b85b693e8f7516e9eae
hellboy2310 <bhaveshpurohit2310@gmail.com>
2022-06-29 02:19:03 +0530

    customized map and customized filter

---
+++ functionalprogramming/polyfills.js
@@ -0,0 +1,39 @@
+
+// //cutomized map
+// let myArr = [1,2,3,4,5];
+// function mypolyfills(arr,cb){
+//     let newArr = [];
+//     for(let i =0;i<arr.length;i++)
+//     {
+//         newArr.push(cb(arr[i]));
+//     }
+//     return newArr;
+// }
+
+// function square(x){
+//     return x * x;
+// }
+
+// console.log(mypolyfills(myArr,square));
+
+//customizede filter
+
+let farr = [1,2,3,4,5,6,7,8,9,10];
+
+function mypolyfillsfilter(arr,cb)
+{
+    let filteredArr = [];
+    for(let i = 0;i<arr.length;i++)
+    {
+        if(cb(arr[i]))
+            filteredArr.push(arr[i]);
+    }
+    return filteredArr;
+}
+
+function iseven(x)
+{
+    if( x % 2 == 0) return x;
+}
+
+console.log(mypolyfillsfilter(farr,iseven));

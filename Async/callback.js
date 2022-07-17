9923ed7f928caf67ef037a9cefe0552b33566953
hellboy2310 <bhaveshpurohit2310@gmail.com>
2022-07-17 17:55:10 +0530

    asynchronous callback

---
+++ Async/callback.js
@@ -0,0 +1,14 @@
+console.log("hello");
+
+setTimeout(function st1(){
+    console.log('I am st1');
+},2000)
+
+setTimeout(function st2(){
+console.log(" I am st2");
+},1000)
+
+function sayBye(){
+    console.log("bye");
+}
+sayBye();
